a1e3d91ec0754c01e9605eeaf4b9558f6e8c8e5b
Javier Cunat <javiercunnat@gmail.com>
2022-09-13 23:23:53 -0400

    First commit for FlashcardApp

---
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.flashcardapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+
+            }
+        }
+    }
