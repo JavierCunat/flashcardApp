e087d7126a6a45907e30d4d3b230d2e6eecfb923
Javier Cunat <javiercunnat@gmail.com>
2022-09-18 23:43:39 -0400

    Completed Lab 2
    Finished Flashcard App Lab 2 CodePath

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -4,17 +4,56 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* ------------------- Question and Answer Visibility Function ------------------- */
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
         flashcardQuestion.setOnClickListener {
             flashcardQuestion.visibility = View.INVISIBLE
             flashcardAnswer.visibility = View.VISIBLE
 
-            }
         }
+        /* --------------------- Answer Choice Background Functions ---------------------- */
+
+        val answerChoiceOne = findViewById<View>(R.id.answerchoice_one)
+        answerChoiceOne.setOnClickListener {
+            answerChoiceOne.setBackgroundColor(resources.getColor(R.color.my_red_color, null))
+        }
+
+        val answerChoiceTwo = findViewById<View>(R.id.answerchoice_two)
+        answerChoiceTwo.setOnClickListener {
+            answerChoiceTwo.setBackgroundColor(resources.getColor(R.color.my_red_color, null))
+        }
+
+        val answerChoiceThree = findViewById<View>(R.id.answerchoice_three)
+        answerChoiceThree.setOnClickListener {
+            answerChoiceThree.setBackgroundColor(resources.getColor(R.color.my_red_color, null))
+        }
+
+        val answerChoiceFour = findViewById<View>(R.id.answerchoice_four)
+        answerChoiceFour.setOnClickListener {
+            answerChoiceFour.setBackgroundColor(resources.getColor(R.color.green, null))
+        }
+        /* ------------ Answer Choices Visibility when clicking eye button Function ----------- */
+
+
+        val toggleChoicesVisibility = findViewById<ImageView>(R.id.toggle_choices_visibility)
+        toggleChoicesVisibility.setOnClickListener {
+            toggleChoicesVisibility.setImageResource(R.drawable.eyeclosed_foreground)
+            answerChoiceOne.visibility = View.INVISIBLE
+            answerChoiceTwo.visibility = View.INVISIBLE
+            answerChoiceThree.visibility = View.INVISIBLE
+            answerChoiceFour.visibility = View.INVISIBLE
+
+
+        }
+
     }
+}
+
